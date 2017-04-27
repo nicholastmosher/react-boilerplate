57d704a1734d3960af8f5329e98fe170fdae1075
Nick Mosher <nicholastmosher@gmail.com>
2017-04-27 18:19:02 -0400

    Adds sass configurations.

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import AppContainer from './containers/AppContainer';
 
+import '../stylesheets/main.scss';
+
 render(
   <AppContainer/>,
   document.getElementById('root')
