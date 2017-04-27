309621df1701fec1dee2f567b8adf5bdaa3c0710
Nick Mosher <nicholastmosher@gmail.com>
2017-04-27 19:11:03 -0400

    Fixed placeholder content.

--- src/js/containers/AppContainer.js
+++ src/js/containers/AppContainer.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const AppContainer = (props) => (
   <div>
-    <h1>Hello, {props.name}!</h1>
-    <button onClick={() => props.greet('Jack')}>Jack</button>
-    <button onClick={() => props.greet('Linda')}>Linda</button>
+    <h1>Hello, React!</h1>
   </div>
 );
 
